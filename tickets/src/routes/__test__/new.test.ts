3f3d1789f903ae2b94e7b3f75bba1cd719133d48
gsk-007 <gsingh16742@gmail.com>
2025-01-21 22:32:59 +0530

    feat(tickets): add test and logic for ticket creation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import app from "../../app";
+import { Ticket } from "../../models/ticket";
 
 const URL = "/api/tickets";
 it("has a route handler listening to /api/tickets for post requests", async () => {
@@ -59,4 +60,23 @@ it("returns an error if an invalid price is provided", async () => {
     .expect(400);
 });
 
-it.skip("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const newTicket = {
+    title: "New Title",
+    price: 20,
+  };
+
+  await request(app)
+    .post(URL)
+    .set("Cookie", global.signin())
+    .send(newTicket)
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(newTicket.title);
+  expect(tickets[0].price).toEqual(newTicket.price);
+});
