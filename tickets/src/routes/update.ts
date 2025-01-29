c55844fa1e08f5f80b0d836603cf7660a2e08c7e
gsk-007 <gsingh16742@gmail.com>
2025-01-29 16:20:45 +0530

    feat(tickets): add tickets update publisher and intergrate it with update route

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from "@gsktickets/common";
 import { Ticket } from "../models/ticket";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -14,8 +16,10 @@ router.put(
   "/api/tickets/:id",
   requireAuth,
   [
-    body('title').not().isEmpty().withMessage('Title is required'),
-    body('price').isFloat({gt:0}).withMessage('Price must be provided and must be greater than 0.')
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be provided and must be greater than 0."),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -31,12 +35,19 @@ router.put(
 
     ticket.set({
       title: req.body.title,
-      price: req.body.price
-    })
-    await ticket.save()
+      price: req.body.price,
+    });
+    await ticket.save();
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
-  },
+  }
 );
 
 export { router as updateTicketRouter };
