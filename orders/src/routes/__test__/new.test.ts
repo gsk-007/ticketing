f1b0291e45c0ae4c2480794ec0b05402bba75c81
gsk-007 <gsingh16742@gmail.com>
2025-01-31 09:40:47 +0530

    feat(orders): add test to create a new order

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,57 @@
+import request from "supertest";
+import app from "../../app";
+import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+it("returns an error if the ticket does not exist", async () => {
+  const ticketId = mongoose.Types.ObjectId();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+it("returns an error if the ticket us already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: "sdfsf",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it("reserves a ticket", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  let orders = await Order.find({});
+  expect(orders.length).toEqual(1);
+});
+
+it.todo("Emits an order created event");
