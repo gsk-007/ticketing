1e826d44acc2bf8d12dbf83124fbcb2dbabab93d
gsk-007 <gsingh16742@gmail.com>
2025-02-01 08:38:29 +0530

    feat(orders): add order created and order cancelled publish events to routes

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import app from "../../app";
 import mongoose from "mongoose";
 import { Ticket } from "../../models/ticket";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns an error if the ticket does not exist", async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -13,6 +14,7 @@ it("returns an error if the ticket does not exist", async () => {
     .send({ ticketId })
     .expect(404);
 });
+
 it("returns an error if the ticket us already reserved", async () => {
   const ticket = Ticket.build({
     title: "concert",
@@ -54,4 +56,19 @@ it("reserves a ticket", async () => {
   expect(orders.length).toEqual(1);
 });
 
-it.todo("Emits an order created event");
+it("Emits an order created event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
