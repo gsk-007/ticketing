2b9563880e6bf7aefd05ff5995f752904379f8e9
gsk-007 <gsingh16742@gmail.com>
2025-02-02 16:50:01 +0530

    feat(orders): add optimistic concurrency package and udpated the listeners

--- orders/src/listeners/ticket-updated-listener.ts
+++ orders/src/listeners/ticket-updated-listener.ts
@@ -5,9 +5,13 @@ import { Ticket } from "../models/ticket";
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName: string = this.queueGroupName;
+
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
     const { id, title, price } = data;
-    const ticket = await Ticket.findById(id);
+    const ticket = await Ticket.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
 
     if (!ticket) {
       throw new Error("Ticket not found");
