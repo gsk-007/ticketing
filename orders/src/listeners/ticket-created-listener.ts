2e220f31fe86a205db4bc4a33eed7770088dd795
gsk-007 <gsingh16742@gmail.com>
2025-02-02 15:15:41 +0530

    feat(orders): add ticket created and ticket updated listeners

---
+++ orders/src/listeners/ticket-created-listener.ts
@@ -0,0 +1,19 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketCreatedEvent } from "@gsktickets/common";
+import { Ticket } from "../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName: string = this.queueGroupName;
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = Ticket.build({
+      id,
+      title,
+      price,
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
---
+++ orders/src/listeners/ticket-updated-listener.ts
@@ -0,0 +1,21 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@gsktickets/common";
+import { Ticket } from "../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+  queueGroupName: string = this.queueGroupName;
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+    ticket.set({ title, price });
+
+    await ticket.save();
+
+    msg.ack();
+  }
+}
