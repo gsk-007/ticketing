54b8ee4966dbf69b462d317cf7478047900b2062
gsk-007 <gsingh16742@gmail.com>
2025-02-03 12:10:26 +0530

    feat(orders): add test for ticket created listener

--- orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -3,6 +3,7 @@ import { natsWrapper } from "../../../nats-wrapper";
 import { TicketCreatedListener } from "../ticket-created-listener";
 import mongoose from "mongoose";
 import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
 
 const setup = async () => {
   //  create an instance of the listener
@@ -11,7 +12,7 @@ const setup = async () => {
   // create a fake data event
   const data: TicketCreatedEvent["data"] = {
     id: new mongoose.Types.ObjectId().toHexString(),
-    title: "",
+    title: "concert",
     price: 10,
     userId: new mongoose.Types.ObjectId().toHexString(),
     version: 0,
@@ -27,11 +28,25 @@ const setup = async () => {
 };
 
 it("creates and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+
   // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
   // write assertions to make sure a ticket was created!
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
 });
 
 it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
   // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
   // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
 });
