8b3c2f3394148d9f56c84930f8f5e221793c32ca
gsk-007 <gsingh16742@gmail.com>
2025-02-03 15:53:23 +0530

    feat(orders): add test for ticket update listener

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,72 @@
+import { TicketUpdatedEvent } from "@gsktickets/common";
+import { natsWrapper } from "../../../nats-wrapper";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+
+const setup = async () => {
+  //  create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // Create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  // create a fake data event
+  const data: TicketUpdatedEvent["data"] = {
+    id: ticket.id,
+    title: "new concert",
+    price: 999,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: ticket.version + 1,
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it("finds, updates, and saves a ticket", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // find the udpated Ticket
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(updatedTicket).toBeDefined();
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function is called
+  expect(msg.ack).toHaveBeenCalled();
+});
+
+it("does not call ack if the event has a future version number", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  data.version = 10;
+
+  try {
+    await listener.onMessage(data, msg);
+  } catch (error) {}
+
+  expect(msg.ack).not.toHaveBeenCalled();
+});
