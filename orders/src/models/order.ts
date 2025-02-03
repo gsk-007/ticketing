0c0030aa449d68913d21fbf0bc7a0188eb267128
gsk-007 <gsingh16742@gmail.com>
2025-02-03 16:11:18 +0530

    feat(orders): add version field to orders model and updated routes

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,6 @@
-import { OrderStatus } from "@gsktickets/common";
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { OrderStatus } from "@gsktickets/common";
 import { TicketDoc } from "./ticket";
 
 interface OrderAttrs {
@@ -14,6 +15,7 @@ interface OrderDoc extends mongoose.Document {
   status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
+  version: number;
 }
 
 interface OrderModel extends mongoose.Model<OrderDoc> {
@@ -49,6 +51,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
