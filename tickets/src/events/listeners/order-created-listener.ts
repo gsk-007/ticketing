9c9a784b97564a03207b622fe16dd4a4164df56b
gsk-007 <gsingh16742@gmail.com>
2025-02-04 08:26:04 +0530

    feat(tickets): add event publish on ticket update, fix(tickets): fix ticket model type

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -25,6 +26,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // Save the Ticket
     await ticket.save();
+    new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     // ack the message
     msg.ack();
